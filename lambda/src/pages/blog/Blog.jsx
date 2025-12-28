35c8ccae3768af380180ae25651b7b1df3078383
curryhowardbadnullpointergood <darkovl10@gmail.com>
2025-12-28 15:11:50 +0000

    update

--- lambda/src/pages/blog/Blog.jsx
+++ lambda/src/pages/blog/Blog.jsx
@@ -8,9 +8,28 @@ function Blog() {
       <h1> Blog: </h1>
 
       <div class="article">
-        
 
+            <p>     My articles focus on the intersection of systems engineering and design. 
+    I explore topics in <strong>Machine Learning</strong>, <strong>Game Theory</strong>, 
+    and <strong>Language Design</strong>, alongside experiments in <strong>Frontend Architecture</strong>. 
+    These articles serve as living documentation of my work and notes, inquiring into 
+    complex systems from first principles.</p>
+            
+
+
+
+      <h3> Aho Corasick</h3>
+      <h3> Life Sync</h3>
+      <h3> Symbolic Regression</h3>
+      <h3> Data Querying Language in Ocaml</h3>
+      <h3> Gaia runtime library</h3>
+      <h3> LifeSync</h3>
+      <h3> Self-Hosting Server</h3>
+      <h3> E-ink alarm</h3>
+      <h3> Video Editing on Linux</h3>
+      
       </div>
+      
     </div>
 
 
--- lambda/src/pages/projects/Projects.jsx
+++ lambda/src/pages/projects/Projects.jsx
@@ -20,27 +20,30 @@ function Projects() {
         <h3>Symbolic Regression Engine:</h3>
 
         <p>This project explores discovering physical laws through syntehtic data, and I use symbolic regression, where I build the engine from scratch in python, then iteratively add pruning stratergies that exploit the physical relationships of equations, in order to efficiently find the correct equation for the dataset. It was later extended to also work in other domains such as biology and finance.</p>
-        <a href="https://github.com/curryhowardbadnullpointergood/lambdatheultimate" target="_blank"> Lambdatheultimate</a>
+        <a href="https://github.com/curryhowardbadnullpointergood/AI-Law-Finder.git" target="_blank"> Symbolic Engine</a>
 
         <h3>LifeSync:</h3>
 
         <p> A GUI calendar project that aims to algorithmically make decisions based on data from wearable devices. Still a long ways off, but I've been making progress. This is literally a sci fi idea, which makes building it a lot of fun. Check out this page to learn more. <a> LINK TO BLOG LIFESYNC UPDATE every now and then. build that into the ui</a></p>
-        <a href="https://github.com/curryhowardbadnullpointergood/lambdatheultimate" target="_blank"> Lambdatheultimate</a>
+
+        <a href="https://github.com/curryhowardbadnullpointergood/LifeSync.git" target="_blank"> LifeSync</a>
 
         <h3>Visualising Differntial Privacy:</h3>
 
         <p> Group project, where I worked with SeCore.ai to produce a visual platform that helps non-technical users understand the advantages of anonmising data through differntial prvacy techniques.</p>
-        <a href="https://github.com/curryhowardbadnullpointergood/lambdatheultimate" target="_blank"> Lambdatheultimate</a>
+
+        <a href="https://github.com/curryhowardbadnullpointergood/SeCore.ai.git" target="_blank"> SeCore.ai</a>
 
         <h3>Ode:</h3>
 
         <p> A social media app that primarily aims to sell and buy tikets for events, and lets users message and text each other. It has authorisation, login, accounts for sellers and users, email verification and 2FA, as well as messaging, event posting, and social media profiles. You can also add and make friends. Primarily used cloud based services to function.</p>
-        <a href="https://github.com/curryhowardbadnullpointergood/lambdatheultimate" target="_blank"> Lambdatheultimate</a>
+        <a href="https://github.com/curryhowardbadnullpointergood/Ode.git" target="_blank"> Ode</a>
 
         <h3>Aho Corasick:</h3>
 
         <p> My favourite string matching algorithm of all time. Implemented in Ocaml. </p>
-        <a href="https://github.com/curryhowardbadnullpointergood/lambdatheultimate" target="_blank"> Lambdatheultimate</a>
+
+        <a href="https://github.com/curryhowardbadnullpointergood/Aho_Corasick.git" target="_blank"> Aho Corasick</a>
 
 
       </div>
